9337f95ce32b8bec2ed859a9785b47fd17495b3d
jeanpaullopes <jean.lopes@ulife.com.br>
2024-09-30 16:55:34 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+//var express = require('express');
+import express from 'express';
+
+//var path = require('path');
+import path from 'path';
+
+//var cookieParser = require('cookie-parser');
+import cookieParser from 'cookie-parser';
+
+//var logger = require('morgan');
+import logger from 'morgan';
+
+//var indexRouter = require('./routes/index');
+import indexRouter from './routes/index.js';
+//var usersRouter = require('./routes/users');
+import usersRouter from './routes/users.js';
+
+
+//workaround for __dirname
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// fim do workaround
+
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+//module.exports = app;
+export default app;
