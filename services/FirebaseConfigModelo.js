c34d2a3e5c04b172653e1d21b635d5a8274c67f6
jeanpaullopes <jean.lopes@ulife.com.br>
2024-09-30 20:32:26 -0300

    modelo de configuração FB

---
+++ services/FirebaseConfigModelo.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// aqui coloquem suas credenciais do firebase
+const firebaseConfig = {
+    apiKey: "adadasd",
+    authDomain: "asdasd.firebaseapp.com",
+    projectId: "asdasd",
+    storageBucket: "adasd-aca2d.appspot.com",
+    messagingSenderId: "234234",
+    appId: "1:2423446:web:66c56d524234234258f643"
+  };
+  export default firebaseConfig;
